4f2b10f6c5aaf3c02cf448d4152bdde2a6f7f3fc
Jess Walters <jessw@iastate.edu>
2016-09-17 10:48:48 -0500

    pls

---
+++ DiscordBot/vote.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+
+
+votes=[]
+voteStrings=[]
+
+async def vote(client, message):
+    global voteStrings
+    text = ""
+    if message.content.startswith('~vote'):
+        text += "Voting has begun make you vote now.\n"
+        inVote = True
+        voteStrings = message.content[5:].split(';')
+        for i, v in enumerate(voteStrings):
+            votes.append(0)
+            text+=("~v" + str(i+1) + ": " + v + "\n")
+            i+=1
+        await client.send_message(message.channel, text)
+
+    elif message.content.startswith('~endvote'):
+        text += "End Vote\n"
+        for i, v in enumerate(voteStrings):
+            text += str(votes[i]) + ": " + v + "\n"
+        voteStrings.clear()
+        votes.clear()
+        await client.send_message(message.channel, text)
+            
+    elif message.content.startswith('~v'):
+        try:
+            votes[int(message.content[2:])-1] += 1
+        except ValueError:
+
+
+
