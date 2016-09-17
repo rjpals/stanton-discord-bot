b611ccbacb7b03933fb73655ab711cd09ead35f9
blindsay <blindsay@iastate.edu>
2016-09-17 00:23:31 -0500

    Added Giphy Command

--- DiscordBot/trash.py
+++ DiscordBot/trash.py
@@ -45,3 +45,12 @@ async def trash(client, message):
         data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/random?tag=silverback+gorilla&api_key=dc6zaTOxFJmzC").read().decode('utf-8'))
         data = json.loads(json.dumps(data['data']))
         await client.send_message(message.channel, data['url'])
+
+    elif message.content.startswith('~giphy'):
+        text = message.content[6:].replace(' ', '+')
+        data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/random?tag="+text+"&api_key=dc6zaTOxFJmzC").read().decode('utf-8'))
+        data = json.loads(json.dumps(data['data']))
+        await client.send_message(message.channel, data['url'])
+
+
+    
