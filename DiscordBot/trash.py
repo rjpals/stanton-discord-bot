db6663ba5d2736c809e98d67eb6169671247a490
blindsay <blindsay@iastate.edu>
2016-09-17 00:04:56 -0500

    harambe

--- DiscordBot/trash.py
+++ DiscordBot/trash.py
@@ -1,5 +1,7 @@
 import discord
 import asyncio
+import urllib
+import json
 
 
 async def trash(client, message):
@@ -19,6 +21,26 @@ async def trash(client, message):
 ░▄▄▄░░▄░░▄░▄░░▄░░▄░░░░▄▄░▄▄░░░▄▄▄░░░▄▄▄
 █▄▄▄█░█▄▄█░█▄▄█░░█░░░█░░█░░█░█▄▄▄█░█░░░█
 █░░░█░░█░░░░█░░░░█░░░█░░█░░█░█░░░█░█░░░█
-        ▀░░░▀░░▀░░░░▀░░░░▀▀▀░░░░░░░░░▀░░░▀░▀▄▄▄▀﻿""")
+▀░░░▀░░▀░░░░▀░░░░▀▀▀░░░░░░░░░▀░░░▀░▀▄▄▄▀﻿""")
         
+    elif message.content.startswith('~kappa'):
+        await client.send_message(message.channel, """░░░░░░░░░
+░░░░▄▀▀▀▀▀█▀▄▄▄▄░░░░
+░░▄▀▒▓▒▓▓▒▓▒▒▓▒▓▀▄░░
+▄▀▒▒▓▒▓▒▒▓▒▓▒▓▓▒▒▓█░
+█▓▒▓▒▓▒▓▓▓░░░░░░▓▓█░
+█▓▓▓▓▓▒▓▒░░░░░░░░▓█░
+▓▓▓▓▓▒░░░░░░░░░░░░█░
+▓▓▓▓░░░░▄▄▄▄░░░▄█▄▀░
+░▀▄▓░░▒▀▓▓▒▒░░█▓▒▒░░
+▀▄░░░░░░░░░░░░▀▄▒▒█░
+░▀░▀░░░░░▒▒▀▄▄▒▀▒▒█░
+░░▀░░░░░░▒▄▄▒▄▄▄▒▒█░
+ ░░░▀▄▄▒▒░░░░▀▀▒▒▄▀░░
+░░░░░▀█▄▒▒░░░░▒▄▀░░░
+░░░░░░░░▀▀█▄▄▄▄▀""")
 
+    elif message.content.startswith('~harambe'):
+        data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/random?tag=silverback+gorilla&api_key=dc6zaTOxFJmzC").read().decode('utf-8'))
+        data = json.loads(json.dumps(data['data']))
+        await client.send_message(message.channel, data['url'])
