b9ea0a95f7cb0e4ad09afdec4d8feeb1276d1a48
Jess Walters <jessw@iastate.edu>
2016-09-16 22:21:24 -0500

    first test code of Fuccbot

---
+++ DiscordBot/Test.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=1000):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, message.author.name + ', you have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run('MTY4NDYyODY0MzcyMzM0NTky.Cr4nmQ.EMPQJiUqo-3rW_OUq6qfMPTaacA')
