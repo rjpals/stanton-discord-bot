fd991023a94d534051657abb217b973d5b61b24f
blindsay <blindsay@iastate.edu>
2016-09-17 22:16:41 -0500

    Changed default help command

--- DiscordBot/help.py
+++ DiscordBot/help.py
@@ -4,4 +4,9 @@ import asyncio
 
 async def help(client, message):
     user = message.author
-    await client.send_message(user, 'fuk u read a book')
+    text = "`MEME COMMANDS\n"
+    text += "~meme\n~meme:textline1:textline2\n~kitten\n~eyebleach\n~shit\n~harambe\n~kappa\n~ayy\n~giphy search\n"
+    text += "\nMODERATION COMMANDS\n~prune num\n~prune num @usernames~kick @username\n~ban @username\n~botname newusername\n~botavatar pngfilepath\n"
+    text += "\nVOICE COMMANDS\n~summon\n~play youtubeurl\n`"
+    text += ""
+    await client.send_message(user, text)
